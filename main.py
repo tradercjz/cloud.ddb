5cf8fe429871a8abadc37b5679678bd3c3517a90
Jinzhi Chen <jizhi.chen@dolphindb.com>
2025-08-27 10:26:33 +0800

    fix(cors): allow all

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from api.v1.api import api_router
 from worker import WorkerSettings
 from db.session import engine
 from db.models import Base # Import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -31,6 +32,17 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+origins = [
+   "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 允许访问的源
+    allow_credentials=True,  # 支持 cookie
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头
+)
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 # Optional: Add a root endpoint
